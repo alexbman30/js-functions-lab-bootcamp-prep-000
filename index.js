710b029d65268e67b5cdc1bdfbac1c8718498001
alexbman30@yahoo.com <alexbman30@yahoo.com>
2018-02-23 20:32:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 // write your code below!
-function happyHoliday(){
+function happyHolidays(){
   return"Happy holidays!"
 }
+happyHoliday("Happy holidays!")
+var name= "you";
 function happyHolidayTo(name){
   return "Happy holiday, ${name}!"
 }
+happyHolidayTo("Happy holiday, ${name}!")
+var holiday= "Independence Day",
+    name = "you";
 function happyHolidayTo(holiday, name){
   return "Happy ${holiday},${name}!"
 }
+happyHolidaysTo(holiday, name)
+var days= 20,
+  holiday= "Mother's Day"
 function holidayCountdown(days, holiday){
     return "Its ${days} days until ${holiday}!"
 }
+holidayCountdown(days,holiday)
+
