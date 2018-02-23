f13a450a17e46be2797660d707e4f597560365fa
alexbman30@yahoo.com <alexbman30@yahoo.com>
2018-02-23 19:26:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 function happyHolidays(){
   console.log('Happy Holidays!')
 }
+happyHolidays('Happy Holidays')
+
 function happyHolidaysTo(name){
   return `Happy Holidays,${name}!`
 }
+happyHolidaysTo('Alex')
+
 function happyHolidaysTo(holiday,name){
   return `Happy ${holiday},${name}!`
 }
+happyHolidaysTo("St.Pattys day","Charlie")
+
 function holidayCountdown(days,holiday){
   return `Its ${days} days until ${holiday}!`
 }
+holidayCount("21","St.Pattys day")
+
